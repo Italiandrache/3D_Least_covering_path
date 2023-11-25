afe2f730b255b57ba896e4188e02aaaa2a08a06b
Edoardo Serra <12883861+serraedoardo@users.noreply.github.com>
2023-11-25 01:44:04 +0100

    CoveringPath.py
    Reworking the algorithm from scratch.

--- CoveringPath.py
+++ CoveringPath.py
@@ -1,91 +1,52 @@
-def grid(row, col, xShift = 0, yShift = 0, subdivisions = 1):
-    xShift, yShift = xShift * subdivisions, yShift * subdivisions
+import math
+
+
+def grid(files, rows, cols = 0, xShift = 0, yShift = 0, zShift = 0, subdivisions = 1):
+    xShift, yShift, zShift = xShift * subdivisions, yShift * subdivisions, zShift * subdivisions
     grid = []
-    for row in range(row*subdivisions):
-        for col in range(col*subdivisions):
-            grid.append(((row + xShift)/subdivisions, (col + yShift)/subdivisions))
+    for file in range(files*subdivisions):
+        for row in range(rows*subdivisions):
+            if cols != 0:
+                for col in range(cols*subdivisions):
+                    grid.append(((file + xShift)/subdivisions, (row + yShift)/subdivisions, (col + zShift)/subdivisions))
+                continue
+            grid.append(((file + xShift)/subdivisions, (row + yShift)/subdivisions, 0.0))
     return grid
 
-def isPointOnLine(p1, p2, p):
-    x1, y1 = p1[0], p1[1]
-    x2, y2 = p2[0], p2[1]
-    px, py = p[0], p[1]
-    if ((px <= x2 and px >= x1) or (px >= x2 and px <= x1)) and ((py <= y2 and py >= y1) or (py >= y2 and py <= y1)):
-        if x1 == x2 or y1 == y2:
-            return True
-        LHS, RHS = py - y1, ((y2 - y1) / (x2 - x1)) * (px - x1)
-        if LHS == RHS:
-            return True
-    return False
-
-def touchedGridPoints(freeGridPoints, p1, p2):
-    touchedGridPoints = []
-    for p in freeGridPoints:
-        if p != p1 and isPointOnLine(p1, p2, p):
-            touchedGridPoints.append(p)
-    return touchedGridPoints
-
-def validLine(freeGridPoints, p1, p2):
-    if len(touchedGridPoints(freeGridPoints, p1, p2)) != 0:
-        return True
-    return False
-
-def writePath(path):
-    f = open("pathFile.txt", "w")
-    pathLen = len(path) - 1
-    f.write(str(pathLen) + "\nis the number of lines.\n")
-    for i in range(pathLen):
-        f.write(str(path[i]) + " ")
-    f.close()
-
-def updateFreeGridPoints(touchedGridPoints, freeGridPoints):
-    for p in touchedGridPoints:
-        for gridPoint in freeGridPoints:
-            if p == gridPoint:
-                freeGridPoints.remove(gridPoint) #Because every point is unique
-                break
-    return freeGridPoints
-
-def isPointOnGrid(freeGridPoints, p):
-    for gridPoint in freeGridPoints:
-        if p == gridPoint:
-            return True
-    return False
-
-def drawLine(currentStartPoint, possiblePoints, currentFreeGridPoints, currentPathLen, currentPath):
-    for p2 in possiblePoints:
-        if currentStartPoint == p2:
+def startGrid(files, rows, cols = 0, xShift = 0, yShift = 0, zShift = 0, subdivisions = 1): #smallest symmetrical element of grid. speeds up computation by avoiding symmetrically equivalent solutions
+    xShift, yShift, zShift = xShift * subdivisions, yShift * subdivisions, zShift*subdivisions
+    grid = []
+    if cols == 0: #2D Grid (on xy plane)
+        for file in range(math.ceil(files*subdivisions/2)):
+            rows_to_iterate = file+1 if rows == files else math.ceil(rows*subdivisions/2)
+            for row in range(rows_to_iterate):
+                grid.append(((file + xShift)/subdivisions, (row + yShift)/subdivisions, 0.0))
+        return grid
+    if files == rows and files == cols: #Cube
+        for file in range(math.ceil(files*subdivisions/2)): #left to right symmetry (1 out of 3 possible planes)
+            for col in range(file+1): #diagonal symmetry (1 out of 2 possible planes from the same opposing face pair)
+                for row in range(col, file+1): #remaining diagonal symmetries (2 out of 4 possible remaining planes of symmetry, one for each opposing face pair)
+                    grid.append(((file + xShift)/subdivisions, (row + yShift)/subdivisions, (col + zShift)/subdivisions))
+        return grid
+    for file in range(math.ceil(file*subdivisions/2)): #Rectangular Parallelepiped
+        if rows == cols or files == rows: #rows==cols or files==rows
+            for col in range(math.ceil(cols*subdivisions/2)): #top to bottom symmetry
+                rows_to_iterate = col+1 if rows == cols else file+1 #diagonal symmetry
+                for row in range(rows_to_iterate):
+                        grid.append(((file + xShift)/subdivisions, (row + yShift)/subdivisions, (col + zShift)/subdivisions))
             continue
-        currentTouchedGridPoints = touchedGridPoints(currentFreeGridPoints, currentStartPoint, p2)
-        if len(currentTouchedGridPoints) == 0:
-            continue
-        currentPath.append(p2) #might give problems on iterations of the same loop (and such the same scope)?
-        f = open("pathFile.txt", "r")
-        minLenLoop = int(f.readline().strip('\n'))
-        f.close()
-        currentPathLen += 1 #same as the above currentPath
-        if currentPathLen >= minLenLoop:
-            break
-        currentFreeGridPoints = updateFreeGridPoints(currentTouchedGridPoints, currentFreeGridPoints)
-        if len(currentFreeGridPoints) == 0:
-            writePath(currentPath)
-            break
-        drawLine(p2, possiblePoints, currentFreeGridPoints, currentPathLen, currentPath)
+        #files==cols or no square faces
+        for row in range(math.ceil(rows*subdivisions/2)):
+            cols_to_iterate = file+1 if files == cols else math.ceil(cols*subdivisions/2)
+            for col in range(cols_to_iterate):
+                grid.append(((file + xShift)/subdivisions, (row + yShift)/subdivisions, (col + zShift)/subdivisions))
+    return grid
 
 
-def main():   
-    f = open("pathFile.txt", "w")
-    f.write("18\n") #upper bound optimal path + 1
-    f.close()
-    currentFreeGridPoints, possiblePoints, currentPath, currentTouchedGridPoints = grid(4, 4), grid(6, 6, -1, -1), [], []
-    currentPathLen = 0
-    for p0 in possiblePoints:
-        if isPointOnGrid(currentFreeGridPoints, p0):
-            currentTouchedGridPoints.append(p0)
-            currentFreeGridPoints = updateFreeGridPoints(currentTouchedGridPoints, currentFreeGridPoints)
-            drawLine(p0, possiblePoints, currentFreeGridPoints, currentPathLen, currentPath)
-            continue
-        drawLine(p0, possiblePoints, currentFreeGridPoints, currentPathLen, currentPath)
+def main():
+    freePointsGrid = grid(4, 4)
+    possiblePointsGrid = grid(6, 6, 0, -1, -1)
+    startingPointsGrid = startGrid(6, 6, 0, -1, -1)
 
 
 if __name__ == '__main__':
